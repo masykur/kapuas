a3febde2609df7793f998e47c21da5879bc82a5a
cahnom@gmail.com <cahnom@gmail.com>
2016-05-24 23:09:40 +0700

    Add mobile web project

---
+++ kapuas/src/kapuas.mobile/Models/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace kapuas.mobile.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+        public string ISBN { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Author { get; set; }
+        public string Description { get; set; }
+        public Category Category { get; set; }
+        public decimal Price { get; set; }
+        public decimal NormalPrice { get; set; }
+    }
+}
---
+++ kapuas/src/kapuas.mobile/Models/Gallery.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace kapuas.mobile.Models
+{
+    public class Gallery
+    {
+        public IEnumerable<Book> Books { get; set; }
+        public IEnumerable<Category> Categories {
+            get { return (from c in Books where c.Category != null select c.Category).Distinct(); }
+        }
+    }
+}
